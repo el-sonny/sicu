3183ccfb2257e04053ddacad8a257817265dda75
urielaero <aero.uriel@gmail.com>
2014-06-14 15:54:18 -0500

    dependencias

--- api/services/DB.js
+++ api/services/DB.js
@@ -5,13 +5,15 @@ module.exports.extractDependencia = function(){
 		var count = Math.round(n/10000)
 		, skip = 0;
 		//for(var i=0;i<count;i++){
-			Solicitud.find().skip(skip).limit(10000).exec(function(err,solicitud){
+			Solicitud.find().skip(skip).limit(100).exec(function(err,solicitud){
 				if(err) throw err;
 				solicitud.forEach(function(v){
 					Dependencia.findOrCreate({nombre:v.DEPENDENCIA},{nombre:v.DEPENDENCIA}).exec(function(err,dependencia){
-						if(err) throw dependencia;
-						console.log(dependencia);
-						//Solicitud.findOne({v.id});
+						if(err) throw err;
+						Solicitud.update({id:v.id},{dependencia:dependencia.id}).exec(function(err,so){
+							if(err) throw err;
+							console.log(so);
+						});
 					});
 				});
 			});
