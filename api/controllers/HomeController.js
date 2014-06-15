a4b14ef0bf6fde7717089fca17fcb720476bc53e
urielaero <aero.uriel@gmail.com>
2014-06-15 11:20:55 -0500

    votes

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -12,10 +12,62 @@ module.exports = {
 		if(req.param('sector')) params.sector = req.param('sector');
 		if(req.param('status')) params.estatus = req.param('status');
 		DB.navQuery(req,function(values){
-			Solicitud.find().where(params).limit(10).sort({'FECHASOLICITUD':'desc'}).exec(function(e,solicitudes){
-				values.solicitudes = solicitudes;
-				res.view(values);
-			});
+			vote(req,res,function(voteup,votedown){
+				Solicitud.find().where(params).limit(10).sort({'FECHASOLICITUD':'desc'}).exec(function(e,solicitudes){
+					values.solicitudes = solicitudes;
+					
+					console.log(req.user);
+					if(req.param('json'))
+						res.json({
+							solicitudes:solicitudes
+							, votes:{
+								up: voteup || req.user && req.user.voteup || []
+								,down:votedown || req.user && req.user.votedown || []
+							}
+						});
+					else
+						res.view(values);
+				});
+			})
 		});
 	},
 };
+
+function vote(req,res,cb){
+	if(req.param('vote') && req.user){
+		Twitter.findOne({nombre:req.user.profile.username}).exec(function(err,user){
+			if(err) throw err;
+			var solicitudId = req.param('solicitudId')
+			, mode = 'vote'+req.param('vote');
+			user[mode] = user[mode] || [];
+			if(user[mode].indexOf(solicitudId)==-1){
+				user[mode].push(solicitudId);
+				var modeDelete = mode=='voteup'?'votedown':'voteup'
+				, deleteItem;
+				user[modeDelete] = user[modeDelete] || [];
+				if((deleteItem = user[modeDelete].indexOf(solicitudId))!=-1){
+					user[modeDelete].splice(deleteItem,1);
+				}
+				user.save(function(err){
+					if(err) throw err;
+					Solicitud.findOne({id:solicitudId}).exec(function(err,solicitud){
+						if(solicitud.vote== undefined){
+							solicitud.vote = 0;
+						}
+						if(mode=='voteup')
+							solicitud.vote++;
+						else
+							solicitud.vote--;
+						solicitud.save(function(err){
+							if(err) throw err;
+							//res.json(solicitudes);
+							cb(user.voteup,user.votedown);
+						});
+					});
+				});
+			}
+		});
+	}else{
+		cb();
+	}
+}
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -37,3 +37,40 @@ app.controller("datepickerCTL", function ($scope) {
   $scope.format = $scope.formats[0];
    
 });
+
+app.controller('votesCTL',function($scope){
+	var updateContent = function(obj){
+		if(obj) jQuery('.down,.up').removeClass('desactive');
+		obj = obj || {};
+		obj.json = true;
+		jQuery.get('/',obj,function(data){
+			var solicitudes = data.solicitudes
+			console.log(data.votes);
+			jQuery('.panel.panel-default').each(function(i,v){
+				console.log(v,solicitudes[i].id);
+				v = jQuery(v);
+				v.next().find('.status .votes strong').text(solicitudes[i].vote || "0");
+				v.find('span.hidden.id').text(solicitudes[i].id);
+				if(data.votes.down && data.votes.down.indexOf(solicitudes[i].id)!=-1){
+					v.find('.down').addClass('desactive');
+				}
+				if(data.votes.up && data.votes.up.indexOf(solicitudes[i].id)!=-1){
+					v.find('.up').addClass('desactive');
+				}
+				
+			});
+		});
+	};
+	
+	jQuery('.vote-action a').on('click',function(e){
+		e.preventDefault();
+		var $this = $(this);
+		updateContent({
+			vote:$this.attr('class')
+			,solicitudId:$this.parent().find('span').text()
+		});
+		
+	})
+
+	updateContent();
+});
