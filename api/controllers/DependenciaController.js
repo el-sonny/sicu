791af19733f166cf73372db245b7208084e4cac4
luis <luis19prz@gmail.com>
2014-06-15 10:56:11 -0500

    icono respuesta y numero de solicitudes por dependencia

--- api/controllers/DependenciaController.js
+++ api/controllers/DependenciaController.js
@@ -8,10 +8,13 @@
 module.exports = {
     index: function(req, res) {
 		Dependencia.findOne({'nombre':req.param('nombre')}).exec(function(e,d){
-			DB.navQuery(req,function(values){
-				values.dependencia = d;
-				res.view(values);
-			});	
+			Solicitud.count({'DEPENDENCIA':req.param('nombre')}).exec(function(err,ns){
+				DB.navQuery(req,function(values){
+					values.dependencia = d;
+					values.nSolicitudes = ns;
+					res.view(values);
+				});	
+			});
 		});
     }
 };
