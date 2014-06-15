fc60456384e1a19b8eb8831baafe0ccdace3fa97
erick <erick@erick.spaceship>
2014-06-14 23:06:29 -0500

    solicitud indiv

--- api/controllers/SolicitudController.js
+++ api/controllers/SolicitudController.js
@@ -7,22 +7,11 @@
 
 module.exports = {
 	    index: function(req, res) {
-			Solicitud.findOne({'FOLIO':parseInt(req.param('id'))}).populate('recurso_revision').exec(function(e,s){
-				console.log(s.recursos_revision);
-				console.log(s.recursos_revision[0]);
-				Dependencia.find({}).sort('nombre').exec(function(e,d){
-					Sector.find({}).sort('nombre').exec(function(e,sec){
-						Estatus.find({}).sort('nombre').exec(function(e,stat){
-							res.view({
-								solicitud:s,
-								dependencias:d,
-								sectores: sec,
-								statii: stat
-							});
-						});
-					});
-					
-				});
+			Solicitud.findOne({'FOLIO':parseInt(req.param('id'))}).populate('recursos_revision').exec(function(e,s){
+				DB.navQuery(req,function(values){
+					values.solicitud = s;
+					res.view(values);
+				});	
 			});
         },
         search: function(req, res){
