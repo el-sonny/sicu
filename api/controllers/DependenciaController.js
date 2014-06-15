2c45f9afd3cc034bd414afc820828a983ce04b71
luis <luis19prz@gmail.com>
2014-06-15 12:40:36 -0500

    fixes perfil dependencia

--- api/controllers/DependenciaController.js
+++ api/controllers/DependenciaController.js
@@ -11,6 +11,7 @@ module.exports = {
 			Solicitud.count({'DEPENDENCIA':req.param('nombre')}).exec(function(err,ns){
 				DB.navQuery(req,function(values){
 					values.dependencia = d;
+					ns = ns.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 					values.nSolicitudes = ns;
 					res.view(values);
 				});	
